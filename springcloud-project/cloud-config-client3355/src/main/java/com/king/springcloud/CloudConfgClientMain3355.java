83c72a6507080c699a0f7a30c4f286c915d8afae
getao <778543972@qq.com>
2022-09-05 16:50:28 +0800

    add: 动态获取git中的config配置信息

--- springcloud-project/cloud-config-client3355/src/main/java/com/king/springcloud/CloudConfgClientMain3355.java
+++ springcloud-project/cloud-config-client3355/src/main/java/com/king/springcloud/CloudConfgClientMain3355.java
@@ -2,14 +2,12 @@ package com.king.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
-public class ConfigCenterMain3344 {
+public class CloudConfgClientMain3355 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigCenterMain3344.class, args);
+        SpringApplication.run(CloudConfgClientMain3355.class, args);
     }
 }
